a5fba6301b756a6ad6cfb54093e7c076890157c5
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-11-19 22:13:36 +0545

    refactoring button component types 🖲️

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { ComponentProps } from 'react';
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ComponentProps<'button'> {}
 
 export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ ...props }, ref) => {
