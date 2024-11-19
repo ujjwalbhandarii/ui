383614db726b58db8d4d1cc62f9b4a80ae0011c3
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-11-19 22:22:46 +0545

    chore: reconfigured vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
-import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, './src/components/index.ts'),
+      name: 'UI',
+      fileName: 'ui',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          'react': 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
 });
