bcf430c962c9db7a39f4f3a9abade02812315b82
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-11-30 19:16:53 +0545

    chore: update on button active state

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -4,14 +4,16 @@ import { cva, VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/cn';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-semibold transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gradients-1 focus-visible:ring-offset-1',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-semibold transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 focus-visible:outline-none focus-visible:ring-2 focus:ring-2 ring-offset-1 ease-in-out duration-200',
   {
     variants: {
       variant: {
         default:
-          'bg-primary-1 hover:bg-primary-2 active:bg-gradients-1 text-shades-1 hover:bg-primary/90',
-        secondary: 'bg-shades-2 text-shades-1 hover:bg-secondary/80',
-        tertiary: 'border border-shades-2 bg-shades-2/5 text-shades-2',
+          'bg-primary-1 hover:bg-primary-1/90 text-shades-1 ring-gradients-1 ',
+        secondary:
+          'bg-shades-2 text-shades-1 hover:bg-shades-2/90 ring-shades-2',
+        tertiary:
+          'border border-shades-2 hover:bg-shades-2/10 bg-shades-2/5 text-shades-2 focus:ring-0 focus-visible:ring-0',
       },
       size: {
         'full-width': 'rounded-lg px-8 py-3 w-full',
